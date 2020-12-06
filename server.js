aea1325da6ddacf2691722a40595546043afde6a
zzr <519019814@qq.com>
2020-12-06 17:57:16 +0800

    新首页

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-let http = require('http')
-let fs = require('fs')
-let url = require('url')
-let port = process.argv[2]
-let log = console.log
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+var log = console.log
 
 if(!port){
     console.log('请指定端口号')
     process.exit(1)
 }
 
-let server = http.createServer(function(request, response){
-    let parsedUrl = url.parse(request.url,true)
-    let path = request.url
-    let query = ''
+var server = http.createServer(function(request, response){
+    var parsedUrl = url.parse(request.url,true)
+    var path = request.url
+    var query = ''
     if(path.indexOf('?') >= 0){ query = path.substring(path.indexOf('?'))}
-    let pathNoQuery = parsedUrl.pathname
-    let queryObject = parsedUrl.query
-    let method = request.method
+    var pathNoQuery = parsedUrl.pathname
+    var queryObject = parsedUrl.query
+    var method = request.method
 })
 
 log('得到HTTP路径\n' + path)
